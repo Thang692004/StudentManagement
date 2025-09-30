8e7d3b6f08c5b0814039f746c00be1884be8b843
Thang692004 <121618852+Thang692004@users.noreply.github.com>
2025-09-30 17:33:34 +0700

    update 2 file code, test

---
+++ StudentManagement.Core/Control.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace StudentManagement
+{
+    public class Control
+    {
+        private string connStr = "Server=localhost;Port=3306;Database=quanlysinhvien;Uid=root;Pwd=thang692004;";
+
+        public void GetUsers()
+        {
+            using (var conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+
+                string query = "SELECT * FROM users"; // bảng của bạn
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                var reader = cmd.ExecuteReader();
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write($"{reader.GetName(i)} | ");
+                }
+                Console.WriteLine();
+                while (reader.Read())
+                {
+                    // In các giá trị của những cột còn lại
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        Console.Write($"{reader.GetValue(i)} | ");
+                    }
+
+                    Console.WriteLine(); // Xuống dòng cho mỗi bản ghi
+                }
+
+            }
+        }
+    }
+}
---
+++ StudentManagement.Core/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StudentManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tạo đối tượng Control và gọi hàm GetUsers
+            Control control = new Control();
+            control.GetUsers();
+
+            // Dừng màn hình để xem kết quả
+            Console.WriteLine("Nhấn Enter để thoát...");
+            Console.ReadLine();
+        }
+    }
+}
